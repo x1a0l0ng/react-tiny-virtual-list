26f2f454b779d8ca9772f6a48ac0f4103c82b12d
albertshaw <albertshaw@outlook.com>
2018-06-21 23:39:08 +0800

    add scroll alignment

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,32 +1,58 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-import VirtualList from '../../src/';
-import './demo.css';
+import VirtualList from '../../src/'
+import './demo.css'
 
 class Demo extends React.Component {
-  renderItem = ({style, index}: {style: React.CSSProperties, index: number}) => {
+  state = {
+    scrollIdx: 1,
+  }
+
+  renderItem = ({
+    style,
+    index,
+  }: {
+    style: React.CSSProperties
+    index: number
+  }) => {
     return (
       <div className="Row" style={style} key={index}>
         Row #{index}
       </div>
-    );
+    )
   }
 
   render() {
+    const { scrollIdx } = this.state
     return (
       <div className="Root">
+        <input
+          type="text"
+          value={scrollIdx}
+          onChange={e => {
+            this.setState({
+              scrollIdx: e.target.value,
+            })
+          }}
+          onBlur={() => {
+            this.refs.scroller['scrollAnimTo'](this.refs.scroller['getOffsetForIndex'](Number(scrollIdx)))
+          }}
+        />
         <VirtualList
+          ref="scroller"
           width="auto"
-          height={400}
+          height={300}
           itemCount={1000}
           renderItem={this.renderItem}
-          itemSize={50}
+          itemSize={60}
           className="VirtualList"
+          shouldScrollAlign={true}
+          scrollToAlignment="center"
         />
       </div>
-    );
+    )
   }
 }
 
-ReactDOM.render(<Demo />, document.querySelector('#app'));
+ReactDOM.render(<Demo />, document.querySelector('#app'))
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import SizeAndPositionManager, {ItemSize} from './SizeAndPositionManager';
+import * as React from 'react'
+import * as PropTypes from 'prop-types'
+import SizeAndPositionManager, { ItemSize } from './SizeAndPositionManager'
 import {
   ALIGNMENT,
   ALIGN_AUTO,
@@ -16,70 +16,88 @@ import {
   positionProp,
   scrollProp,
   sizeProp,
-} from './constants';
+} from './constants'
 
 const STYLE_WRAPPER: React.CSSProperties = {
   overflow: 'auto',
   willChange: 'transform',
   WebkitOverflowScrolling: 'touch',
-};
+}
 
 const STYLE_INNER: React.CSSProperties = {
   position: 'relative',
   overflow: 'hidden',
   width: '100%',
   minHeight: '100%',
-};
+}
 
-const STYLE_ITEM: {position: 'absolute', left: number, width: string} = {
+const STYLE_ITEM: { position: 'absolute'; left: number; width: string } = {
   position: 'absolute',
   left: 0,
   width: '100%',
-};
+}
+
+const rAFCancel = (() => {
+  return cancelAnimationFrame || webkitCancelAnimationFrame || clearTimeout
+})()
+
+const rAF = (() => {
+  return (
+    requestAnimationFrame ||
+    webkitRequestAnimationFrame ||
+    function(callback) {
+      window.setTimeout(callback, 1000 / 60)
+    }
+  )
+})()
+
+const easeInOutCubic = x =>
+  x < 0.5 ? 4 * x * x * x : (x - 1) * (2 * x - 2) * (2 * x - 2) + 1
 
 export interface ItemStyle {
-  position: 'absolute',
-  top?: number,
-  left: number,
-  width: string | number,
-  height?: number,
+  position: 'absolute'
+  top?: number
+  left: number
+  width: string | number
+  height?: number
 }
 
 interface StyleCache {
-  [id: number]: ItemStyle,
+  [id: number]: ItemStyle
 }
 
 export interface ItemInfo {
- index: number,
- style: ItemStyle,
+  index: number
+  style: ItemStyle
 }
 
 export interface RenderedRows {
-  startIndex: number,
-  stopIndex: number,
+  startIndex: number
+  stopIndex: number
 }
 
 export interface Props {
-  className?: string,
-  estimatedItemSize?: number,
-  height: number | string,
-  itemCount: number,
-  itemSize: ItemSize,
-  overscanCount?: number,
-  scrollOffset?: number,
-  scrollToIndex?: number,
-  scrollToAlignment?: ALIGNMENT,
-  scrollDirection?: DIRECTION,
-  style?: any,
-  width?: number | string,
-  onItemsRendered?({startIndex, stopIndex}: RenderedRows): void,
-  onScroll?(offset: number, event: React.UIEvent<HTMLDivElement>): void,
-  renderItem(itemInfo: ItemInfo): React.ReactNode,
+  className?: string
+  estimatedItemSize?: number
+  height: number | string
+  itemCount: number
+  itemSize: ItemSize
+  overscanCount?: number
+  scrollOffset?: number
+  scrollToIndex?: number
+  scrollToAlignment?: ALIGNMENT
+  scrollDirection?: DIRECTION
+  style?: any
+  width?: number | string
+  shouldScrollAlign?: boolean
+  onItemsRendered?({ startIndex, stopIndex }: RenderedRows): void
+  onScroll?(offset: number, event: React.UIEvent<HTMLDivElement>): void
+  renderItem(itemInfo: ItemInfo): React.ReactNode
 }
 
 export interface State {
-  offset: number,
-  scrollChangeReason: SCROLL_CHANGE_REASON,
+  offset: number
+  scrollChangeReason: SCROLL_CHANGE_REASON
 }
 
 export default class VirtualList extends React.PureComponent<Props, State> {
@@ -87,49 +105,68 @@ export default class VirtualList extends React.PureComponent<Props, State> {
     overscanCount: 3,
     scrollDirection: DIRECTION_VERTICAL,
     width: '100%',
-  };
+    shouldScrollAlign: false,
+  }
 
   static propTypes = {
     estimatedItemSize: PropTypes.number,
-    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+      .isRequired,
     itemCount: PropTypes.number.isRequired,
-    itemSize: PropTypes.oneOfType([PropTypes.number, PropTypes.array, PropTypes.func]).isRequired,
+    itemSize: PropTypes.oneOfType([
+      PropTypes.number,
+      PropTypes.array,
+      PropTypes.func,
+    ]).isRequired,
     onItemsRendered: PropTypes.func,
     overscanCount: PropTypes.number,
     renderItem: PropTypes.func.isRequired,
     scrollOffset: PropTypes.number,
     scrollToIndex: PropTypes.number,
-    scrollToAlignment: PropTypes.oneOf([ALIGN_AUTO, ALIGN_START, ALIGN_CENTER, ALIGN_END]),
-    scrollDirection: PropTypes.oneOf([DIRECTION_HORIZONTAL, DIRECTION_VERTICAL]).isRequired,
+    scrollToAlignment: PropTypes.oneOf([
+      ALIGN_AUTO,
+      ALIGN_START,
+      ALIGN_CENTER,
+      ALIGN_END,
+    ]),
+    scrollDirection: PropTypes.oneOf([DIRECTION_HORIZONTAL, DIRECTION_VERTICAL])
+      .isRequired,
     width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-  };
+    shouldScrollAlign: PropTypes.bool,
+  }
+
+  scrollAnimation = 0
+
+  delayAlignment = 0
+
+  scrollAnimating = false
 
   sizeAndPositionManager = new SizeAndPositionManager({
     itemCount: this.props.itemCount,
-    itemSizeGetter: (index) => this.getSize(index),
+    itemSizeGetter: index => this.getSize(index),
     estimatedItemSize: this.getEstimatedItemSize(),
-  });
+  })
 
   state = {
-    offset: (
+    offset:
       this.props.scrollOffset ||
-      this.props.scrollToIndex != null && this.getOffsetForIndex(this.props.scrollToIndex) ||
-      0
-    ),
+      (this.props.scrollToIndex != null &&
+        this.getOffsetForIndex(this.props.scrollToIndex)) ||
+      0,
     scrollChangeReason: SCROLL_CHANGE_REQUESTED,
-  };
+  }
 
-  private rootNode: HTMLElement;
+  private rootNode: HTMLElement
 
-  private styleCache: StyleCache = {};
+  private styleCache: StyleCache = {}
 
   componentDidMount() {
-    const {scrollOffset, scrollToIndex} = this.props;
+    const { scrollOffset, scrollToIndex } = this.props
 
     if (scrollOffset != null) {
-      this.scrollTo(scrollOffset);
+      this.scrollTo(scrollOffset)
     } else if (scrollToIndex != null) {
-      this.scrollTo(this.getOffsetForIndex(scrollToIndex));
+      this.scrollTo(this.getOffsetForIndex(scrollToIndex))
     }
   }
 
@@ -141,16 +178,14 @@ export default class VirtualList extends React.PureComponent<Props, State> {
       scrollOffset,
       scrollToAlignment,
       scrollToIndex,
-    } = this.props;
-    const scrollPropsHaveChanged = (
+    } = this.props
+    const scrollPropsHaveChanged =
       nextProps.scrollToIndex !== scrollToIndex ||
       nextProps.scrollToAlignment !== scrollToAlignment
-    );
-    const itemPropsHaveChanged = (
+    const itemPropsHaveChanged =
       nextProps.itemCount !== itemCount ||
       nextProps.itemSize !== itemSize ||
       nextProps.estimatedItemSize !== estimatedItemSize
-    );
 
     if (
       nextProps.itemCount !== itemCount ||
@@ -159,112 +194,193 @@ export default class VirtualList extends React.PureComponent<Props, State> {
       this.sizeAndPositionManager.updateConfig({
         itemCount: nextProps.itemCount,
         estimatedItemSize: this.getEstimatedItemSize(nextProps),
-      });
+      })
     }
 
     if (itemPropsHaveChanged) {
-      this.recomputeSizes();
+      this.recomputeSizes()
     }
 
     if (nextProps.scrollOffset !== scrollOffset) {
       this.setState({
         offset: nextProps.scrollOffset || 0,
         scrollChangeReason: SCROLL_CHANGE_REQUESTED,
-      });
+      })
     } else if (
-      typeof nextProps.scrollToIndex === 'number' && (
-        scrollPropsHaveChanged || itemPropsHaveChanged
-      )
+      typeof nextProps.scrollToIndex === 'number' &&
+      (scrollPropsHaveChanged || itemPropsHaveChanged)
     ) {
       this.setState({
-        offset: this.getOffsetForIndex(nextProps.scrollToIndex, nextProps.scrollToAlignment, nextProps.itemCount),
+        offset: this.getOffsetForIndex(
+          nextProps.scrollToIndex,
+          nextProps.scrollToAlignment,
+          nextProps.itemCount
+        ),
         scrollChangeReason: SCROLL_CHANGE_REQUESTED,
-      });
+      })
     }
   }
 
   componentDidUpdate(_: Props, prevState: State) {
-    const {offset, scrollChangeReason} = this.state;
+    const { offset, scrollChangeReason } = this.state
 
-    if (prevState.offset !== offset && scrollChangeReason === SCROLL_CHANGE_REQUESTED) {
-      this.scrollTo(offset);
+    if (
+      prevState.offset !== offset &&
+      scrollChangeReason === SCROLL_CHANGE_REQUESTED
+    ) {
+      this.scrollTo(offset)
     }
   }
 
-  handleScroll = (e: React.UIEvent<HTMLDivElement>)  => {
-    const {onScroll} = this.props;
-    const offset = this.getNodeOffset();
+  findAlignedIndex = (offset: number) => {
+    const {
+        scrollDirection = DIRECTION_VERTICAL,
+        scrollToAlignment,
+      } = this.props,
+      size = this.props[sizeProp[scrollDirection]]
+    let calcOffset = offset
+    switch (scrollToAlignment) {
+      case ALIGN_END:
+        calcOffset += size
+        break
+      case ALIGN_CENTER:
+        calcOffset += Math.round(size / 2)
+        break
+      default:
+    }
+    return this.sizeAndPositionManager.findNearestItem(calcOffset)
+  }
 
-    if (offset < 0 || this.state.offset === offset || e.target !== this.rootNode) {
-      return;
+  handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const { onScroll, shouldScrollAlign } = this.props
+    const offset = this.getNodeOffset()
+
+    if (
+      offset < 0 ||
+      this.state.offset === offset ||
+      e.target !== this.rootNode
+    ) {
+      return
     }
 
     this.setState({
       offset,
       scrollChangeReason: SCROLL_CHANGE_OBSERVED,
-    });
+    })
+
+    if (shouldScrollAlign && !this.scrollAnimating) {
+      clearTimeout(this.delayAlignment)
+      this.delayAlignment = setTimeout(() => {
+        this.scrollAnimTo(this.getOffsetForIndex(this.findAlignedIndex(offset)))
+      }, 100)
+    }
 
     if (typeof onScroll === 'function') {
-      onScroll(offset, e);
+      onScroll(offset, e)
     }
   }
 
   getEstimatedItemSize(props = this.props) {
-    return props.estimatedItemSize || typeof props.itemSize === 'number' && props.itemSize || 50;
+    return (
+      props.estimatedItemSize ||
+      (typeof props.itemSize === 'number' && props.itemSize) ||
+      50
+    )
   }
 
   getNodeOffset() {
-    const {scrollDirection = DIRECTION_VERTICAL} = this.props;
-    return this.rootNode[scrollProp[scrollDirection]];
+    const { scrollDirection = DIRECTION_VERTICAL } = this.props
+    return this.rootNode[scrollProp[scrollDirection]]
   }
 
   scrollTo(value: number) {
-    const {scrollDirection = DIRECTION_VERTICAL} = this.props;
-    this.rootNode[scrollProp[scrollDirection]] = value;
+    const { scrollDirection = DIRECTION_VERTICAL } = this.props
+    this.rootNode[scrollProp[scrollDirection]] = value
   }
 
-  getOffsetForIndex(index: number, scrollToAlignment = this.props.scrollToAlignment, itemCount: number = this.props.itemCount): number {
-    const {scrollDirection = DIRECTION_VERTICAL} = this.props;
+  scrollAnimTo(targetPos: number) {
+    rAFCancel(this.scrollAnimation)
+    this.scrollAnimating = true
+    const { scrollDirection = DIRECTION_VERTICAL } = this.props,
+      startTime = new Date().getTime(),
+      startPos = this.getNodeOffset(),
+      targetOffset = Math.round(targetPos - startPos),
+      duration = Math.max(
+        Math.abs(
+          Math.round(
+            (2000 * targetOffset) / this.sizeAndPositionManager.getTotalSize()
+          )
+        ),
+        200
+      ),
+      animateScroll = () => {
+        const progress = new Date().getTime() - startTime,
+          percent =
+            progress >= duration ? 1 : easeInOutCubic(progress / duration)
+
+        this.rootNode[scrollProp[scrollDirection]] =
+          startPos + Math.ceil(targetOffset * percent)
+        if (percent < 1) {
+          this.scrollAnimation = rAF(animateScroll)
+          return
+        } else {
+          this.scrollAnimating = false
+        }
+      }
+    animateScroll()
+  }
+
+  getOffsetForIndex(
+    index: number,
+    scrollToAlignment = this.props.scrollToAlignment,
+    itemCount: number = this.props.itemCount
+  ): number {
+    const { scrollDirection = DIRECTION_VERTICAL } = this.props
 
     if (index < 0 || index >= itemCount) {
-      index = 0;
+      index = 0
     }
 
     return this.sizeAndPositionManager.getUpdatedOffsetForIndex({
       align: scrollToAlignment,
       containerSize: this.props[sizeProp[scrollDirection]],
-      currentOffset: this.state && this.state.offset || 0,
+      currentOffset: (this.state && this.state.offset) || 0,
       targetIndex: index,
-    });
+    })
   }
 
   getSize(index: number) {
-    const {itemSize} = this.props;
+    const { itemSize } = this.props
 
     if (typeof itemSize === 'function') {
-      return itemSize(index);
+      return itemSize(index)
     }
 
-    return Array.isArray(itemSize) ? itemSize[index] : itemSize;
+    return Array.isArray(itemSize) ? itemSize[index] : itemSize
   }
 
   getStyle(index: number) {
-    const style = this.styleCache[index];
-    if (style) { return style; }
+    const style = this.styleCache[index]
+    if (style) {
+      return style
+    }
 
-    const {scrollDirection = DIRECTION_VERTICAL} = this.props;
-    const {size, offset} = this.sizeAndPositionManager.getSizeAndPositionForIndex(index);
+    const { scrollDirection = DIRECTION_VERTICAL } = this.props
+    const {
+      size,
+      offset,
+    } = this.sizeAndPositionManager.getSizeAndPositionForIndex(index)
 
-    return this.styleCache[index] = {
+    return (this.styleCache[index] = {
       ...STYLE_ITEM,
       [sizeProp[scrollDirection]]: size,
       [positionProp[scrollDirection]]: offset,
-    };
+    })
   }
 
   recomputeSizes(startIndex = 0) {
-    this.styleCache = {};
-    this.sizeAndPositionManager.resetItem(startIndex);
+    this.styleCache = {}
+    this.sizeAndPositionManager.resetItem(startIndex)
   }
 
   render() {
@@ -283,42 +399,56 @@ export default class VirtualList extends React.PureComponent<Props, State> {
       scrollToAlignment,
       style,
       width,
-      ...props,
-    } = this.props;
-    const {offset} = this.state;
-    const {start, stop} = this.sizeAndPositionManager.getVisibleRange({
+      ...props
+    } = this.props
+    const { offset } = this.state
+    const { start, stop } = this.sizeAndPositionManager.getVisibleRange({
       containerSize: this.props[sizeProp[scrollDirection]] || 0,
       offset,
       overscanCount,
-    });
-    const items: React.ReactNode[] = [];
+    })
+    const items: React.ReactNode[] = []
 
     if (typeof start !== 'undefined' && typeof stop !== 'undefined') {
       for (let index = start; index <= stop; index++) {
-        items.push(renderItem({
-          index,
-          style: this.getStyle(index),
-        }));
+        items.push(
+          renderItem({
+            index,
+            style: this.getStyle(index),
+          })
+        )
       }
 
       if (typeof onItemsRendered === 'function') {
         onItemsRendered({
           startIndex: start,
           stopIndex: stop,
-        });
+        })
       }
     }
 
     return (
-      <div ref={this.getRef} {...props} onScroll={this.handleScroll} style={{...STYLE_WRAPPER, ...style, height, width}}>
-        <div style={{...STYLE_INNER, [sizeProp[scrollDirection]]: this.sizeAndPositionManager.getTotalSize()}}>
+      <div
+        ref={this.getRef}
+        {...props}
+        onScroll={this.handleScroll}
+        style={{ ...STYLE_WRAPPER, ...style, height, width }}
+      >
+        <div
+          style={{
+            ...STYLE_INNER,
+            [sizeProp[
+              scrollDirection
+            ]]: this.sizeAndPositionManager.getTotalSize(),
+          }}
+        >
           {items}
         </div>
       </div>
-    );
+    )
   }
 
   private getRef = (node: HTMLDivElement): void => {
-    this.rootNode = node;
+    this.rootNode = node
   }
 }
